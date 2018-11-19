b96b183aed9d30dfb46bf621025f76386f7b0676
MCClarke <mcarlotclarke@gmail.com>
2018-11-19 12:01:21 -0500

    Test Driven Katas next sepcs

--- wrap.js
+++ wrap.js
@@ -2,12 +2,17 @@
 
 // if str is empty
 // if str.length is less than numCol
+
 function wrap (str, numCol){
     if (str.length === 0){
         return '';
     } else if (str.length < numCol){
         return str;
+    } else if (str.length === numCol){
+        return str;
     }
 }
+
+// console.log('Lorem ipsum dolor \n sit eu amet, elit na \n magna sem amet nulla \n vel purus ac ligula.')
 
 module.exports = wrap;
--- wrap.spec.js
+++ wrap.spec.js
@@ -10,7 +10,10 @@ describe('wrap', () => {
   it('Returns string if the string length is less than column number', () => {
     expect(wrap("Lorem", 10)).to.equal("Lorem");
   });
+  it('Returns string if the string length is equal to column number', () => {
+    expect(wrap("Lorem", 5)).to.equal("Lorem");
+  });
+  it('Returns a string with line breaks inserted at the right places', () => {
+    expect(wrap("Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.", 20)).to.equal('Lorem ipsum dolor \n sit eu amet, elit na \n magna sem amet nulla \n vel purus ac ligula.');
+  });
 });
-
- 
-  
